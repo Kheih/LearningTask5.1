495028b2e4e385215398b918e80acde72c991fe4
Kheih <56195049+Kheih@users.noreply.github.com>
2020-10-10 22:04:47 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,361 @@
+<svg width="200" height="300" style="border:1px solid black">
+  
+  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+  
+    <circle cx="50" cy="200" r="10" stroke="green" stroke-width="4" fill="yellow" />
+  
+   <circle cx="50" cy="150" r="10" stroke="green" stroke-width="4" fill="yellow" />
+  
+   <circle cx="50" cy="250" r="10" stroke="green" stroke-width="4" fill="yellow" />
+  
+   <circle cx="25" cy="125" r="20" stroke="green" stroke-width="4" fill="yellow" />
+  
+   <circle cx="75" cy="175" r="20" stroke="green" stroke-width="4" fill="yellow" />
+  
+  <circle cx="25" cy="225" r="20" stroke="green" stroke-width="4" fill="yellow" />
+  
+    <circle cx="75" cy="275" r="20" stroke="green" stroke-width="4" fill="yellow" />
+  
+   <circle cx="150" cy="250" r="40" stroke="black" stroke-width="4" fill="red" />
+  
+  <circle cx="150" cy="150" r="10" stroke="black" stroke-width="4" fill="red" />
+  
+    <circle cx="150" cy="100" r="10" stroke="black" stroke-width="4" fill="red" />
+  
+  <circle cx="150" cy="50" r="10" stroke="black" stroke-width="4" fill="red" />
+  
+   <circle cx="125" cy="25" r="20" stroke="black" stroke-width="4" fill="red" />
+  
+   <circle cx="175" cy="75" r="20" stroke="black" stroke-width="4" fill="red" />
+  
+    <circle cx="125" cy="125" r="20" stroke="black" stroke-width="4" fill="red" />
+  
+  <circle cx="175" cy="175" r="20" stroke="black" stroke-width="4" fill="red" />
+  
+  <line x1="100" y1="280" x2="100" y2="20" style="stroke:pink;stroke-width:2" />
+  
+    <line x1="0" y1="100" x2="100" y2="100" style="stroke:pink;stroke-width:2" />
+  
+  <line x1="200" y1="200" x2="100" y2="200" style="stroke:pink;stroke-width:2" />
+
+    <line x1="50" y1="290" x2="50" y2="110" style="stroke:pink;stroke-width:2" />
+  
+  <line x1="20" y1="150" x2="80" y2="150" style="stroke:pink;stroke-width:2" />
+  
+  <line x1="20" y1="200" x2="80" y2="200" style="stroke:pink;stroke-width:2" />
+  
+   <line x1="20" y1="250" x2="80" y2="250" style="stroke:pink;stroke-width:2" />
+  
+  <line x1="150" y1="10" x2="150" y2="190" style="stroke:pink;stroke-width:2" />
+  
+     <line x1="120" y1="50" x2="180" y2="50" style="stroke:pink;stroke-width:2" />
+  
+  <line x1="120" y1="100" x2="180" y2="100" style="stroke:pink;stroke-width:2" />
+  
+    <line x1="120" y1="150" x2="180" y2="150" style="stroke:pink;stroke-width:2" />
+
+</svg>
+<svg width="200" height="300" style="border:1px solid black">
+  
+<rect x="0" y="0" width="100" height="300" style="fill:yellow; stroke:white; stroke-width:1"/>
+                            
+<rect x="100" y="0" width="100" height="300" style="fill:black; stroke:white; stroke-width:1"/>
+  
+  <circle cx="12" cy="10" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="12" cy="50" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="12" cy="90" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="12" cy="130" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="12" cy="170" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="12" cy="210" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="12" cy="250" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="12" cy="290" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+     <circle cx="37" cy="270" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="37" cy="230" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="37" cy="190" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+    <circle cx="37" cy="110" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="37" cy="70" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="37" cy="30" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+     <circle cx="37" cy="150" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+     <circle cx="62" cy="10" r="5" stroke="black" stroke-width="4" fill="black" />
+
+  <circle cx="62" cy="50" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="62" cy="90" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="62" cy="130" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="62" cy="170" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="62" cy="210" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="62" cy="250" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="62" cy="290" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="88" cy="270" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="88" cy="230" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="88" cy="190" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="88" cy="150" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="88" cy="110" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+   <circle cx="88" cy="70" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="88" cy="30" r="5" stroke="black" stroke-width="4" fill="black" />
+  
+    <circle cx="112" cy="10" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="112" cy="50" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="112" cy="90" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="112" cy="130" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="112" cy="170" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  
+  <circle cx="112" cy="210" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="112" cy="250" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="112" cy="290" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="30" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="70" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="110" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="150" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="190" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="230" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="137" cy="270" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="162" cy="30" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="162" cy="70" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+   <circle cx="162" cy="110" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+   <circle cx="162" cy="150" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+   <circle cx="162" cy="190" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+   <circle cx="162" cy="230" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="162" cy="270" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+   <circle cx="188" cy="290" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="250" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="210" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="170" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="130" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="90" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="50" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  <circle cx="188" cy="10" r="5" stroke="yellow" stroke-width="1" fill="yellow" />
+  
+</svg>
+
+<svg width="200" height="300" style="border:1px solid black">
+  
+  
+    <polyline points="80,0 80,40,120 40,120 80,160 80,160 120, 200 120,"
+  style="fill:none;stroke:red;stroke-width:4" />
+  
+
+    <polyline points="0,40 40,40 40,80 80,80 80,120 120,120 120,160 160, 160 160, 200 200,200 200"
+  style="fill:none;stroke:black;stroke-width:4" />
+  
+    <polyline points="0,120 40,120 40,160 80,160 80,200 120,200 120,240 160, 240 160,280 200,280 200"
+  style="fill:none;stroke:red;stroke-width:4" />
+  
+  <polyline points="0,200 40,200 40,240 80,240 80,280 120,280 120,320 160, 320 160"
+  style="fill:none;stroke:black;stroke-width:4" />
+  
+  <circle cx="95" cy="25" r="10" stroke="red" stroke-width="4" fill="red" />
+   <circle cx="135" cy="65" r="10" stroke="red" stroke-width="4" fill="red" />
+   <circle cx="175" cy="105" r="10" stroke="red" stroke-width="4" fill="red" />
+   <circle cx="145" cy="95" r="10" stroke="red" stroke-width="4" fill="red" />
+    <circle cx="105" cy="55" r="10" stroke="red" stroke-width="4" fill="red" />
+  
+ <circle cx="95" cy="105" r="10" stroke="black" stroke-width="4" fill="black" />
+  <circle cx="25" cy="55" r="10" stroke="black" stroke-width="4" fill="black" />
+  <circle cx="55" cy="65" r="10" stroke="black" stroke-width="4" fill="black" />
+  <circle cx="65" cy="95" r="10" stroke="black" stroke-width="4" fill="black" />
+  <circle cx="105" cy="135" r="10" stroke="black" stroke-width="4" fill="black" />
+   <circle cx="135" cy="145" r="10" stroke="black" stroke-width="4" fill="black" />
+   <circle cx="145" cy="175" r="10" stroke="black" stroke-width="4" fill="black" />
+   <circle cx="175" cy="185" r="10" stroke="black" stroke-width="4" fill="black" />
+  
+  <circle cx="175" cy="265" r="10" stroke="red" stroke-width="4" fill="red" />
+    <circle cx="145" cy="255" r="10" stroke="red" stroke-width="4" fill="red" />
+  <circle cx="135" cy="225" r="10" stroke="red" stroke-width="4" fill="red" />
+   <circle cx="105" cy="215" r="10" stroke="red" stroke-width="4" fill="red" />
+    <circle cx="95" cy="185" r="10" stroke="red" stroke-width="4" fill="red" />
+   <circle cx="65" cy="175" r="10" stroke="red" stroke-width="4" fill="red" />
+    <circle cx="55" cy="145" r="10" stroke="red" stroke-width="4" fill="red" />
+   <circle cx="25" cy="135" r="10" stroke="red" stroke-width="4" fill="red" />
+  
+  <circle cx="25" cy="215" r="10" stroke="black" stroke-width="4" fill="black" />
+   <circle cx="55" cy="225" r="10" stroke="black" stroke-width="4" fill="black" />
+   <circle cx="65" cy="255" r="10" stroke="black" stroke-width="4" fill="black" />
+   <circle cx="95" cy="265" r="10" stroke="black" stroke-width="4" fill="black" />
+  
+</svg>
+
+<svg width="200" height="300" style="border:1px solid black">
+  
+     <ellipse cx="70" cy="25" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+  <ellipse cx="50" cy="55" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+  <ellipse cx="70" cy="85" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+    <ellipse cx="130" cy="125" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+    <ellipse cx="150" cy="155" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+   <ellipse cx="130" cy="185" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+  
+   <ellipse cx="70" cy="215" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+   <ellipse cx="50" cy="245" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+   <ellipse cx="70" cy="275" rx="30" ry="15" fill="yellow" stroke-width="1" stroke="rgb(0,0,0)"/>
+  
+    
+  <polygon points="150,20 190,40 190,80 150,100 110,80 110,40" fill="black"/>
+    <polygon points="50,110 90,130 90,170 50,190 10,170 10,130" fill="black"/>
+     <polygon points="150,200 190,220 190,260 150,280 110,260 110,220" fill="black"/>
+  
+</svg>
+
+<svg width="200" height="300" style="border:1px solid black">
+  
+<rect x="0" y="0" width="100" height="300" style="fill:pink; stroke:pink; stroke-width:1"/>
+                            
+<rect x="100" y="0" width="100" height="300" style="fill:black; stroke:black; stroke-width:1"/>
+  
+ <line x1="50" y1="300" x2="50" y2="150" style="stroke:red;stroke-width:5" />
+   <line x1="25" y1="300" x2="25" y2="200" style="stroke:red;stroke-width:5" />
+  <line x1="75" y1="300" x2="75" y2="100" style="stroke:red;stroke-width:5" />
+  
+   <line x1="125" y1="200" x2="125" y2="0" style="stroke:white;stroke-width:5" />
+  
+  <line x1="150" y1="150" x2="150" y2="0" style="stroke:white;stroke-width:5" />
+  
+   <line x1="175" y1="100" x2="175" y2="0" style="stroke:white;stroke-width:5" />
+  
+    <circle cx="175" cy="120" r="20" stroke="white" stroke-width="1" fill="white" />
+  
+   <circle cx="150" cy="170" r="20" stroke="white" stroke-width="1" fill="white" />
+  
+   <circle cx="125" cy="220" r="20" stroke="white" stroke-width="1" fill="white" />
+  
+  <circle cx="50" cy="140" r="20" stroke="red" stroke-width="1" fill="red" />
+  
+   <circle cx="25" cy="190" r="20" stroke="red" stroke-width="1" fill="red" />
+  
+  <circle cx="75" cy="90" r="20" stroke="red" stroke-width="1" fill="red" />
+  
+   <circle cx="75" cy="120" r="10" stroke="red" stroke-width="1" fill="red" />
+  
+   <circle cx="75" cy="135" r="5" stroke="red" stroke-width="1" fill="red" />
+  
+   <circle cx="50" cy="170" r="10" stroke="red" stroke-width="1" fill="red" />
+  
+   <circle cx="50" cy="185" r="5" stroke="red" stroke-width="1" fill="red" />
+  
+  <circle cx="25" cy="220" r="10" stroke="red" stroke-width="1" fill="red" />
+  
+   <circle cx="25" cy="235" r="5" stroke="red" stroke-width="1" fill="red" />
+  
+</svg>
+
+<svg width="200" height="300" style="border:1px solid black">
+
+    <polyline points="0,40 40,40 40,80 80,80 80,120 120,120 120,160 160, 160 160, 200 200,200 200"
+  style="fill:black;stroke:red;stroke-width:4" />
+  
+    <polyline points="0,120 40,120 40,160 80,160 80,200 120,200 120,240 160, 240 160,280 200,280 200"
+  style="fill:black;stroke:red;stroke-width:4" />
+  
+  <polygon points="150,20 190,40 190,80 150,100 110,80 110,40" fill="yellow"stroke= "red"/> 
+
+   <polygon points="50,200 90,220 90,260 50,280 10,260 10,220" fill="yellow"stroke= "red"/> 
+
+</svg>
+<svg width="200" height="300" style="border:1px solid black">
+  
+    <polyline points="80,0 80,40,120 40,120 80,160 80,160 120, 200 120,"
+  style="fill:none;stroke:red;stroke-width:4" />
+  
+     <polyline points="0,40 40,40 40,80 80,80 80,120 120,120 120,160 160, 160 160, 200 200,200 200"
+  style="fill:none;stroke:red;stroke-width:4" />
+  
+  
+<rect x="90" y="10" width="20" height="20"
+ style="fill:skyblue;stroke:pink;stroke-width:5"/>
+  
+  <rect x="130" y="10" width="20" height="20"
+ style="fill:skyblue;stroke:pink;stroke-width:5"/>
+  
+   <rect x="170" y="10" width="20" height="20"
+ style="fill:skyblue;stroke:pink;stroke-width:5"/>
+
+  <rect x="130" y="50" width="20" height="20"
+ style="fill:skyblue;stroke:pink;stroke-width:5"/>
+  
+  <rect x="170" y="50" width="20" height="20"
+ style="fill:skyblue;stroke:pink;stroke-width:5"/>
+  
+   <rect x="170" y="90" width="20" height="20"
+ style="fill:skyblue;stroke:pink;stroke-width:5"/>
+
+    
+<rect x="0" y="42" width="40" height="300" style="fill:black"/>
+                            
+  <rect x=39 y="82" width="40" height="300" style="fill:pink"/>
+  
+<rect x="78" y="122" width="40" height="300" style="fill:black" />
+  
+  <rect x="118" y="162" width="40" height="300" style="fill:pink" />
+  
+  <rect x="158" y="202" width="42" height="300" style="fill:black" />
+  
+    <ellipse cx="40" cy="19" rx="30" ry="15" fill="skyblue" stroke-width="1" stroke="red"/>
+  
+   <ellipse cx="80" cy="60" rx="30" ry="15" fill="skyblue" stroke-width="1" stroke="red"/>
+  
+     <ellipse cx="120" cy="100" rx="30" ry="15" fill="skyblue" stroke-width="1" stroke="red"/>
+  
+       <ellipse cx="160" cy="140" rx="30" ry="15" fill="skyblue" stroke-width="1" stroke="red"/>
+  
+   <circle cx="20" cy="65" r="15" stroke="red" stroke-width="1" fill="white" />
+  
+   <circle cx="20" cy="90" r="10" stroke="red" stroke-width="1" fill="white" />
+  
+   <circle cx="20" cy="105" r="5" stroke="red" stroke-width="1" fill="white" />
+  
+    <circle cx="60" cy="105" r="15" stroke="white" stroke-width="1" fill="red" />
+  
+   <circle cx="60" cy="130" r="10" stroke="white" stroke-width="1" fill="red" />
+  
+   <circle cx="60" cy="145" r="5" stroke="white" stroke-width="1" fill="red" />
+  
+     <circle cx="100" cy="145" r="15" stroke="red" stroke-width="1" fill="white" />
+  
+   <circle cx="100" cy="170" r="10" stroke="red" stroke-width="1" fill="white" />
+  
+   <circle cx="100" cy="185" r="5" stroke="red" stroke-width="1" fill="white" />
+  
+  <circle cx="140" cy="185" r="15" stroke="white" stroke-width="1" fill="red" />
+  
+   <circle cx="140" cy="210" r="10" stroke="white" stroke-width="1" fill="red" />
+  
+   <circle cx="140" cy="225" r="5" stroke="white" stroke-width="1" fill="red" />
+  
+   <circle cx="180" cy="225" r="15" stroke="red" stroke-width="1" fill="white" />
+  
+   <circle cx="180" cy="250" r="10" stroke="red" stroke-width="1" fill="white" />
+  
+   <circle cx="180" cy="265" r="5" stroke="red" stroke-width="1" fill="white" />
+  
+ <polygon points="50,150 90,170 90,210 50,230 10,210 10,170"  stroke="red" stroke-width="1" fill="white"/>
+  
+       <polygon points="50,210 90,230 90,270 50,290 10,270 10,230"  stroke="red" stroke-width="1" fill="white"/>
+  
+  
+</svg>
